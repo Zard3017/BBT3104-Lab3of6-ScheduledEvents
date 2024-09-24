b5b0811084f72ec4dc26fdf005287d89774e2235
Bradley  Lusalenge <123534693+Zard3017@users.noreply.github.com>
2024-09-24 12:33:43 +0300

    Update lab_submission.sql
    Question 1
    
    Signed-off-by: Bradley  Lusalenge <123534693+Zard3017@users.noreply.github.com>

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,8 @@
+CREATE TABLE `customer_service_kpi` (
+`customer_service_KPI_timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`customer_service_KPI_average_waiting_time_minutes` INT NOT NULL,
+PRIMARY KEY (`customer_service_KPI_timestamp`));
+
+
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+
